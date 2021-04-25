fbdd8e0563bd87d0a57c35a9c96526fa7bddd6b7
nilay <nilaynarlawar9211@gmail.com>
2021-04-25 16:27:00 -0700

    updated test file unit and BDD

---
+++ test/test_model.py
@@ -0,0 +1,96 @@
+from models import Employee
+from pytest import raises
+from models import EmpSalary
+
+
+def test_exists():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+def test_get_all_employees():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+def test_add_employee():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+def test_update_employee():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+def test_delete_employee():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+#------------- Salary Unit test case ------------
+
+def test_exists_salary():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+def test_get_all_employees_salaries():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+def test_add_employee_salary():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+def test_update_employee_salary():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
+
+def test_delete_employee_salary():
+    # Arrange
+
+    # Act
+
+    # Assert
+
+    return True
