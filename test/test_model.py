85a20b739d3439d3a09bf1fa5837468673f5ea2d
jlakshmi0505 <jlakshmi@dons.usfca.edu>
2021-04-30 08:17:54 -0700

    test_model

--- test/test_model.py
+++ test/test_model.py
@@ -1,26 +1,27 @@
 import mock
 
+from app import add_employee
 from models import Employee, EmpSalary
 
 
 def test_add_employee():
     mock_employee = mock.Mock(spec=Employee)
-    mock_employee.add_employee.return_value = True
-    actual = mock_employee.add_employee('b', 'b@gmail.com', 'USA', 'Google')
+    actual = add_employee('b', 'b@gmail.com', 'USA', 'Google')
+    mock_employee.save.assert_called()
     assert actual
 
 
 def test_update_employee():
     mock_employee = mock.Mock(spec=Employee)
-    mock_employee.update_employee.return_value = True
     actual = mock_employee.update_employee(1, 'b1', 'b@gmail.com', 'USA-101', 'Facebook')
+    mock_employee.save.assert_called()
     assert actual
 
 
 def test_delete_employee():
     mock_employee = mock.Mock(spec=Employee)
-    mock_employee.delete_employee.return_value = True
     actual = mock_employee.delete_employee(1)
+    mock_employee.delete.assert_called()
     assert actual
 
 
@@ -29,21 +30,22 @@ def test_delete_employee():
 
 def test_add_employee_salary():
     mock_employee_salary = mock.Mock(spec=EmpSalary)
-    mock_employee_salary.add_employee_salary.return_value = True
     actual = mock_employee_salary.add_employee_salary(1, "100", "USD", "Deposit", "Biweekly")
+    mock_employee_salary.save.assert_called()
+
     assert actual
 
 
 def test_update_employee_salary():
     mock_employee_salary = mock.Mock(spec=EmpSalary)
-    mock_employee_salary.update_employee_salary.return_value = True
     actual = mock_employee_salary.update_employee_salary(1, "500", "USD", "Deposit", "Biweekly")
+    mock_employee_salary.save.assert_called()
     assert actual
 
 
 def test_delete_employee_salary():
     mock_employee_salary = mock.Mock(spec=EmpSalary)
-    mock_employee_salary.delete_employee_salary.return_value = True
     actual = mock_employee_salary.delete_employee_salary(1)
+    mock_employee_salary.delete.assert_called()
     assert actual
 
